354542a1f05583c1d57dc1538ceba661ba13e023
Leonid <l.gofshteyn@gmail.com>
2023-08-23 19:45:26 +0300

    Инициализирован Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('PANTA-REI Project')
+    .setDescription('Документация REST API проекта PANTA-REI')
+    .setVersion('1.0')
+    .addTag('API')
+    .build()
+  ;
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(configService.get('port'));
 }
 bootstrap();
